4fbdc80f01303030d28a2b765887eccc76b665d2
dkkaran1996 <dkkaran1996@gmail.com>
2023-07-18 12:38:53 +0530

    VACWAC

---
+++ Products/VACWAC.php
@@ -0,0 +1,312 @@
+<?php
+include "../assets/load.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="description" content="VAC SOFTTECK has a range of products tailor made to suit customer requirements like IP PBX , Contact Center Solutions, Call Center Solutions, IVR, Voice Logger, Dialer and a powerful CRM that can cater to any vertical. The supply chain is supported efficiently by a 24/7 strong support team.">
+    <meta name="keywords" content="VACSoftteck, Good tele communication company, Good service company, Good customer support">
+
+
+    <title>VAC Softteck</title>
+    <!-- Favicons -->
+    <link href="../assets/img/favicon.ico" rel="icon">
+    <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="../assets/css/style.css" rel="stylesheet">
+    <link href="../assets/css/vacwac.css" rel="stylesheet">
+
+</head>
+
+<body style="background-color: #f7f8fc;">
+    <main class="products-container">
+        <section id="Products" class="portfolio section-bg">
+            <?php
+            load_template('_product_header');
+            ?>
+
+            <!-- ======= Product Section Start ======= -->
+
+
+            <section id="Products" class="portfolio section-bg py-5">
+                <div class="container">
+                    <div id="VACWAC" class="section-title">
+                        <h2>VAC Whatsap Connect</h2>
+                        <hr class="bg-dark mx-auto mt-0 rounded " style="width: 90px; height: 5px;">
+                    </div>
+                    <div class="row g-5">
+                        <div class="col-lg-5 wow fadeInRight py-5 bg-primary imageContainer" data-aos="fade-left" style="border-radius: 20px;">
+                            <!-- <div class="section-title position-relative pb-3 mb-4 wow fadeInRight " data-aos="fade-down"> -->
+                                <!-- <h2 class="mt-2 text-start text-light">VAC Whatsapp Connect:</h2>
+                            </div>
+                            <p class="mb-0 text-light fs-5 " data-aos="fade-down">Our objective is to ensure that this platform is being used by Professionals,  Tiny Units, MSMEs and Corporates
+seamlessly with their existing calling platform and CRM / ERP.
+                            <p>
+                            <ul class="fs-5 text-light" data-aos="fade-up">
+                                <li>Send bulk campaigns</li>
+                                <li>Automate notifications</li>
+                                <li>Have Multiple users</li>
+                                <li>Based on key words divert messages to the respective executives automatically</li>
+                                <li>Chatbot to answer questions</li>
+                                <li>Get real-time delivery insights with MIS</li>
+                                <li>reports & Analytics </li>
+                                <li>Send updates, delivery reminders, payment reminders & other notifications using our APIs</li>
+                            </ul> -->
+                            <img src="../images/vacwac.png">
+                        </div>
+                        <div class="col-lg-7 wow fadeInUp py-5 pb-0" data-aos="fade-right">
+                            <div class="section-title position-relative pb-3 mb-4 wow fadeInRight" data-aos="fade-down">
+                                <h2 class="mt-2 text-start text-dark">VAC Whatsap Connect :</h2>
+                            </div>
+                            <p class="mb-3  fs-5" data-aos="fade-right">Our objective is to ensure that this platform is being used by Professionals,  Tiny Units, MSMEs and Corporates seamlessly with their existing calling platform and CRM / ERP.</p>
+                            <ul class="fs-5" data-aos="fade-up">
+                                <li>Send bulk campaigns</li>
+                                <li>Automate notifications</li>
+                                <li>Have Multiple users</li>
+                                <li>Based on key words divert messages to the respective executives automatically</li>
+                                <li>Chatbot to answer questions</li>
+                                <li>Get real-time delivery insights with MIS</li>
+                                <li>reports & Analytics </li>
+                                <li>Send updates, delivery reminders, payment reminders & other notifications using our APIs</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+            <!--  PRICING-->
+        <section class='content'>
+          <div class="container">
+            <div class="row-fluid clearfix" >
+              
+              <div class="pricing-wrapper comparison-table clearfix style-3">
+                <div class="col-md-4 pricing-col list-feature">
+                  <div class="pricing-card">
+                    <div class="pricing-header">
+                      <h5>Choose Your Plan</h5>
+                      <p>Compare Package Feature</p>
+                    </div>
+                    <div class="pricing-feature">
+                      <li>
+                        <p>Contacts</p>
+                      </li>
+                      <li>
+                        <p>Chats</p>
+                      </li>
+                      <li>
+                        <p>Campaigns</p>
+                      </li>
+                      <li>
+                        <p>Templates</p>
+                      </li>
+                      <li>
+                        <p>Additional Template</p>
+                      </li>
+                      <li>
+                        <p>Keywords</p>
+                      </li>
+                      <li>
+                        <p>Users</p>
+                      </li>
+                      <li>
+                        <p>Additional Users</p>
+                      </li>
+                      <li>
+                        <p>Onetime Setup Cost</p>
+                      </li>
+                      <li>
+                        <p>API Integration</p>
+                      </li>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 pricing-col person">
+                  <div class="pricing-card">
+                    <div class="pricing-header">
+                      <h5><strong>Engage Plan</strong></h5>
+                      <div class="price-box">
+                        <div class="price">1475
+                          <div class="currency">&#8377;</div>
+                          <div class="plan">/ Mo</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="pricing-feature">
+                      <li>
+                        <p>
+                          <span>Unlimited</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Unlimited</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Unlimited</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>5</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>-</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>50</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>3</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>-</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Rs.9999/-</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Based on Requirement</span>
+                        </p>
+                      </li>
+                    </div>
+                    <div class="pricing-footer">
+                      <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_M4uVFw8iKI0nZT" async> </script> </form>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 pricing-col current unlim">
+                  <div class="pricing-card">
+                    <div class="pricing-header">
+                      <h5><strong>Elite Plan</strong></h5>
+                      <a href="" class="ribbon">
+                        <i class="fa fa-star"></i>
+                        <span>Elite</span>
+                      </a>
+                      <div class="price-box">
+                        <div class="price">2750
+                          <div class="currency">&#8377;</div>
+                          <div class="plan">/ Mo</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="pricing-feature">
+                      <li>
+                        <p>
+                          <span>Unlimited</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Unlimited</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Unlimited</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>30</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Rs.250/-</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Unlimited</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>10</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Rs.500/-</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Rs.14999/-</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>
+                          <span>Based on Requirement</span>
+                        </p>
+                      </li>
+                    </div>
+                    <div class="pricing-footer">
+                      <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_M4uc15lm1dp6gX" async> </script> </form>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </section>
+        <div id="note">
+            <p><strong>Note:</strong> 18% GST Extra. <strong> Conversation Cost : </strong> Direct Recharge with Meta.</p>
+        </div>
+
+            <!-- ======= Product Section End ======= -->
+
+            <!-- ======= Footer ======= -->
+            <?php
+            load_template('_Footer');
+            ?>
+        </section>
+    </main>
+    <!-- End Footer -->
+
+    <!-- Vendor JS Files -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+
+    <script src="../assets/vendor/aos/aos.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="../assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="../assets/js/main.js"></script>
+    <!-- API Configuration -->
+    <script src="../assets/js/msg.js" type="text/javascript"></script>
+    <script src="../assets/js/lightbox-plus-jquery.js" type="text/javascript"></script>
+    <script src="../assets/js/popup.js" type="text/javascript"></script>
+</body>
+
+</html>
