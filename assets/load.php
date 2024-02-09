50209947a2e011db76b33ad02de778fd358e72c6
Kirubhakaran <57623979+dkkaran1996@users.noreply.github.com>
2024-02-09 12:30:16 +0530

    Revert "Revert "Ui Updation""

--- assets/load.php
+++ assets/load.php
@@ -4,9 +4,11 @@ function load_template($name)
 {
 
     include $_SERVER['DOCUMENT_ROOT'] . "/assets/_templates/$name.php"; //not consider //super global
+    // include $_SERVER['DOCUMENT_ROOT'] . "/Vacsoft-Website/vacsoftteck/assets/_templates/$name.php"; 
 }
 function load_templatejs($name)
 {
 
     include $_SERVER['DOCUMENT_ROOT'] . "/assets/_templates/$name.js"; //not consider //super global
+    // include $_SERVER['DOCUMENT_ROOT'] . "/Vacsoft-Website/vacsoftteck/assets/_templates/$name.js";
 }
